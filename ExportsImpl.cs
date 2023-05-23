26727c7c81f779bb0a27bca0c9f6388269d8ecdc
yowl <scott.waye@hubse.com>
2023-05-23 08:13:17 -0500

    first attempt to make something compile

---
+++ ExportsImpl.cs
@@ -0,0 +1,25 @@
+// See https://aka.ms/new-console-template for more information
+using wit_the_world;
+
+internal class ExportsImpl : IExports
+{
+    public void Float32Param(float p0)
+    {
+        throw new NotImplementedException();
+    }
+
+    public float Float32Result()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Float64Param(double p0)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Float64Result()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using wit_the_world;
+
+Console.WriteLine("Hello, World!");
+
+ExportsInterop.Exports = new ExportsImpl();
+
+Imports.float32Param(1.23f);
+
+Imports.float64Param(3.45d);
+
+Console.WriteLine(Imports.float32Result());
+
+Console.WriteLine(Imports.float64Result());
